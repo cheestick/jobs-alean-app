75dd6c002206ce1e84695e7524fcd5600a0c7d9b
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-11-16 22:25:11 +0200

    ssr fetch was implemented

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -1,5 +1,36 @@
-const Jobs = () => {
-  return (<h1>Jobs List</h1>)
+import { TJobList } from '../../types'
+
+export async function getServerSideProps() {
+  const response = await fetch(
+    process.env.API_PATH as string,
+    {
+      headers:
+      {
+        'Authorization': `Bearer ${process.env.API_BEARER_TOKEN}`,
+      }
+    })
+ 
+  const data: TJobList = await response.json()
+  
+  if (!data) return { notFound: true }
+  
+  return {
+    props: { data }
+  }
+}
+
+type TJobsProps = {
+  data: TJobList
+}
+
+const Jobs = ({ data }: TJobsProps) => {
+  
+  return (
+    <>
+      <h1>Jobs List</h1>
+      {data.map((job) => <div key={job.id}>{ job.name}</div>)}
+    </>
+  )
 }
 
 export default Jobs
