896de654a754f1ae900f489dcb1306a7dff8555c
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-11-19 13:37:42 +0200

    project colors were added

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,80 @@ module.exports = {
         proximaNova: ["Proxima Nova", "sans-serif"],
         roboto: ["Roboto", "sans-serif"],
       },
+      colors: {
+        avatar: {
+          DEFAULT: "#D8D8D8",
+        },
+        badge: {
+          benefits: {
+            fill: "#FFCF0026", //rgba(255, 207, 0, 0.15);
+            name: "#988B49",
+            stroke: "#FFCF00",
+          },
+          type: {
+            fill: "#A1B1DB4D", // rgba(161, 177, 219, 0.317343);
+            name: "#55699E",
+            stroke: "#55699E4D", //rgba(85, 105, 158, 0.3);
+          },
+        },
+        bookmark: {
+          DEFAULT: "#70778B",
+          unmarked: "#70778B",
+          marked: "##38415D",
+        },
+        button: {
+          DEFAULT: "#384564",
+          arrow: "#384564",
+          backdrop: "#384564", // opacity: 0.14;
+          name: "#3A4562",
+        },
+        divider: {
+          DEFAULT: "#3A4562",
+        },
+        "job-bar": {
+          DEFAULT: "#EFF0F5",
+          white: "#FFFFFF",
+        },
+        list: {
+          mark: "#384564A1", // rgba(56, 69, 100, 0.632594);
+        },
+        location: {
+          DEFAULT: "#D8D8D8",
+        },
+        main: {
+          DEFAULT: "#3A4562",
+          secondary: "#878D9D",
+          backdrop: "#E6E9F2",
+          transparent: "#38415DD1", //rgba(56, 65, 93, 0.82);
+          "on-dark": "#E7EAF0",
+          "on-dark-secondary": "#E8EBF3",
+          "on-dark-transparent": "#FFFFFFAB", // rgba(255, 255, 255, 0.669635);
+        },
+        map: {
+          fill: "#2A3047",
+          "dark-fill": "#202336",
+        },
+        pagination: {
+          accent: "#5876C5",
+          arrows: "#7D859C",
+          DEFAULT: "#70778B",
+          divider: "DEE3EF",
+          mobileBg: "#F9FAFD",
+          mobileTxt: "#38415D9A", //  rgba(56, 65, 93, 0.602109);
+          mobileAccent: "##55699E",
+        },
+        raiting: {
+          DEFAULT: "#384564",
+          uset: "#384564",
+          set: "#38415D",
+          stroke: "#3A4562",
+        },
+        timestamp: {
+          DEFAULT: "#878D9D",
+          transparent: "#38415D5B", //rgba(56, 65, 93, 0.355988);
+          "transparent-mobile": "#38415D9A", //rgba(56, 65, 93, 0.602109);
+        },
+      },
     },
   },
   plugins: [],
