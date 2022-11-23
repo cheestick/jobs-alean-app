9b99e13ef03fde8ec8e26dab55c399a7a9a02a22
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-11-23 09:54:50 +0200

    JobBar was changed according to decomposed components

--- components/JobBar.tsx
+++ components/JobBar.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react'
 import { IJobDetails } from 'types'
 import Bookmark from './Bookmark/Bookmark'
+import CompanyBriefInfo from './CompanyBriefInfo'
 import Raiting from './Rating/'
 import Timestamp from './Timestamp'
 
@@ -9,7 +10,7 @@ interface IJobBarProps {
 }
 
 const JobBar: FC<IJobBarProps> = ({ jobDetails }) => {
-  const { id, title, address, name, updatedAt, pictures: [avatarURL] } = jobDetails
+  const { id, updatedAt } = jobDetails
   const [rating, setRating] = useState<number>(0)
   const [bookmarked, setBookmarked] = useState<boolean>(false)
 
@@ -18,30 +19,11 @@ const JobBar: FC<IJobBarProps> = ({ jobDetails }) => {
       data-id={id}
       className='rounded-lg px-4 pt-[13px] pb-[27px] sm:py-6 
         shadow-mobile sm:shadow-desktop sm:text-md
-        bg-job-bar sm:bg-job-bar-white'>
-      <section>
-        <h2 className='text-lg leading-6 -tracking-[0.035rem]
-          sm:text-xl sm:leading-[1.5625rem] sm:-tracking-[0.0391rem]
-          sm:font-bold'>
-          {title}
-        </h2>
-        <picture className='flex'>
-          <img
-            className='w-[66px] sm:w-[85px] aspect-square rounded-full 
-              object-cover object-center'
-            src={avatarURL}
-            alt={`Avatar ${name}`} />
-        </picture>
-        <p className='text-base leading-[1.5625rem] text-main-secondary tracking-[0.015rem]'>
-          Department name &#183; {name}
-        </p>
-        <address className='text-base leading-[1.5625rem] text-main-secondary tracking-[0.015rem]'>
-          {address}
-        </address>
-      </section>
-
+        bg-job-bar sm:bg-job-bar-white'
+    >
+      <CompanyBriefInfo briefInfo={jobDetails} />
       <Raiting rating={rating} setRating={setRating} />
-      <Bookmark icon='flag' text='Save to my list' checked={bookmarked} setChecked={setBookmarked} />
+      <Bookmark icon='flag' checked={bookmarked} setChecked={setBookmarked} />
       <Timestamp time={ updatedAt } />
     </section>   
   )
